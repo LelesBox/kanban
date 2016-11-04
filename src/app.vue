c660f1f94fb71bf9381201d1526526ed38464fa4
LeeBox <li_xiaoliang@foxmail.com>
2016-11-04 16:24:26 +0800

    更新配置

--- src/app.vue
+++ src/app.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 require('normalize.css')
-require('assets/font-awesome-4.7.0/css/font-awesome.min.css')
+// require('assets/font-awesome-4.7.0/css/font-awesome.min.css')
 </script>
 <style lang="scss">
 html, body {
